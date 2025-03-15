b11d6f54e5e001610396c44e37480f94cf5c3762
Levan Didebashvili <levdidebashvili@gmail.com>
2025-03-15 23:06:15 +0400

    feature/ done

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+import "./App.css";
+import WelcomeMessage from "./WelcomeMessage";
+function App() {
+  return <WelcomeMessage />;
+}
+
+export default App;
---
+++ src/WelcomeMessage.jsx
@@ -0,0 +1,9 @@
+export default WelcomeMessage;
+function WelcomeMessage() {
+  return (
+    <div>
+      <h1>Hello, World</h1>
+      <p>WElcome to learning JSX</p>
+    </div>
+  );
+}
